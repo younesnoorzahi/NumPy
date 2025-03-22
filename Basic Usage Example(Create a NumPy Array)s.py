0d1f411c9f07c1ff8165c55cb91d89b539fb09f7
Rexo <younes.noorzai2005@gmail.com>
2025-03-22 21:36:48 +0330

    Create  Basic Usage Example(Create a NumPy Array)s.py

---
+++ Basic Usage Example(Create a NumPy Array)s.py
@@ -0,0 +1,10 @@
+# Create a NumPy Array
+import numpy as np
+
+# Create a 1D array
+arr = np.array([1, 2, 3, 4, 5])
+print("1D Array:", arr)
+
+# Create a 2D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D Array:\n", arr_2d)
